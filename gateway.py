107da4d924b5eadc6cc491970ae75636965f0973
Rick Lupton <r.lupton@gmail.com>
2014-09-08 13:44:24 +0100

    Logging and subscribe/broadcast

--- gateway.py
+++ gateway.py
@@ -1,10 +1,12 @@
+import argparse
 import asyncio
 import serial
 from datetime import datetime
 import json
-from paho.mqtt.client import Client, error_string
+import paho.mqtt.client as paho
 
 import logging
+logger = logging.getLogger('gateway')
 
 import sys
 sys.path.append('..')
@@ -43,15 +45,11 @@ class EmonMQTTGateway:
             nodes = load_definitions_from_yaml(f.read())
         self.parser = FrameParser(nodes)
 
-        self.mqtt_client = Client()
-        def on_connect(client, userdata, rc):
-            print('------- connected to mqtt')
-        self.mqtt_client.on_connect = on_connect
-        def on_log(client, userdata, level, buf):
-            print('******', level, buf)
-        self.mqtt_client.on_log = on_log
-        print('connecting................')
-        print(self.mqtt_client.connect("localhost"))
+        self.mqtt_client = paho.Client()
+        self.mqtt_client.on_connect = self._mqtt_on_connect
+        self.mqtt_client.on_message = self._mqtt_on_message
+        logger.info('Trying to connect to MQTT server...')
+        self.mqtt_client.connect("localhost")
         self._mqtt_loop()
 
     @asyncio.coroutine
@@ -63,18 +61,21 @@ class EmonMQTTGateway:
 
         # Open serial port
         s = serial.Serial('/dev/ttyAMA0', 9600)
-        reader, writer = yield from create_read_write_streams(s)
+        reader, self.writer = yield from create_read_write_streams(s)
 
         while True:
             line = yield from reader.readline()
             time = datetime.utcnow().replace(microsecond=0).isoformat()
+
+            logger.debug('Received line: %s', line)
             self.frame_log.writeline(
                 '{} {}'.format(time, line.decode('ascii').strip()))
-            # print("Line:", repr(line.decode('utf8')))
-            # line = str(self.input_stream.readline(), encoding='ascii')
-            node, values = self.parser.process_frame(line)
+
+            node, values = self.parser.process_frame(line.decode('ascii'))
             if node is None:
                 continue
+            logger.debug('Processed frame: %s %s', node, values)
+
             for k, v in values.items():
                 topic = '{}/{}'.format(node.name, k)
                 payload = json.dumps({
@@ -83,16 +84,57 @@ class EmonMQTTGateway:
                     'units': node.channels[k].get('units', ''),
                     'description': node.channels[k].get('description', ''),
                 })
-                print('publish', topic, payload)
+                logger.debug('Publishing [%s] %s', topic, payload)
                 self.mqtt_client.publish(topic, payload)
 
+    def _send_time(self):
+         now = datetime.now()
+         logger.debug("Broadcasting time %02d:%02d", now.hour, now.minute)
+         frame = "00,%02d,%02d,00,s" % (now.hour, now.minute)
+         self.writer.write(frame.encode('ascii'))
+
+    def _mqtt_on_connect(self, client, userdata, flags_dict, rc):
+        if rc == 0:
+            logger.info('Connected to MQTT server')
+            # subscribe
+            self.mqtt_client.subscribe('/broadcast/#')
+        else:
+            logger.error('Error connecting to MQTT server (%d): %s',
+                         rc, paho.connack_string(rc))
+
+    def _mqtt_on_message(self, client, userdata, message):
+        logger.info('Received MQTT message [%s] "%s"',
+                    message.topic, message.payload)
+        if message.topic == '/broadcast/time':
+            self._send_time()
+
     def _mqtt_loop(self):
-        print('periodically running...')
-        print(self.mqtt_client.loop(timeout=0))
+        rc = self.mqtt_client.loop(timeout=0)
+        if rc != 0:
+            logger.error('Error in MQTT loop (%d): %s',
+                         rc, paho.error_string(rc))
         self._loop.call_later(1.0, self._mqtt_loop)
 
 
-gateway = EmonMQTTGateway()
-loop = asyncio.get_event_loop()
-loop.run_until_complete(gateway.run_input())
-loop.close()
+def main():
+    # Set up logging
+    parser = argparse.ArgumentParser(description='emon gateway')
+    parser.add_argument('-L', '--log-level', default='warning')
+    args = parser.parse_args()
+
+    numeric_level = getattr(logging, args.log_level.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % args.log_level)
+    logging.basicConfig(
+        level=numeric_level,
+        format="%(asctime)s %(name)s [%(levelname)s] %(message)s")
+    logging.getLogger('asyncio').setLevel('WARNING')
+
+    gateway = EmonMQTTGateway()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(gateway.run_input())
+    loop.close()
+
+
+if __name__ == '__main__':
+    main()
